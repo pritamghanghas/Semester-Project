e74d8ab393046cf6b59bfe5a1bc3f9ae5595549b
Marco Zorzi <marcozo91@gmail.com>
2016-04-05 10:26:44 +0200

    Updated all files

--- skye_ros/include/skye_ros/skye_ros.h
+++ skye_ros/include/skye_ros/skye_ros.h
@@ -23,7 +23,7 @@
 #include <gazebo_msgs/GetLinkState.h>
 #include <gazebo_msgs/LinkState.h>
 
-#include "skye_ros/ApplyWrenchCogNed.h"
+#include "skye_ros/ApplyWrenchCogBf.h"
 #include "skye_ros/GetLinkStateNed.h"
 
 namespace skye_ros {
@@ -37,28 +37,30 @@ public:
     SkyeRos();
 
     /**
-     * @brief      Callback function when a new imu msg has been received from Gazebo.
-     * 
-     * Converts Imu data from local ENU frame, attached to the Imu box in Gazebo, to
-     * a local NED frame and publish this new data. 
-     * This NED frame has the same origin of the ENU frame.
+     * @brief      Callback function when a new imu msg has been received from
+     *             Gazebo.
      *
-     * @param[in]  imu_enu  Imu data expressed in local ENU frame, IMU attached to Skye.
+     *             Converts Imu data from local Gazebo frame, attached to the
+     *             Imu box in Gazebo, to a local body frame and publish this new
+     *             data. This body frame has X poitning forward, Y poiting right
+     *             and Z poiting downwards.
+     *
+     * @param[in]  imu_gz_bf_p  imu data in Gazebo body frame.
      */
-    void imuEnuCallback(const sensor_msgs::ImuConstPtr  &imu_enu_p);
+    void imuCallback(const sensor_msgs::ImuConstPtr  &imu_gz_bf_p);
 
     /**
      * @brief      Callback function when apply_wrench_cog service is called.
      * 
-     * Converts the requested wrench, expressed in Skye's NED local frame, to
+     * Converts the requested wrench, expressed in Skye's body frame, to
      * a wrench expressed in Gazebo's ENU world frame and apply it in Gazebo.
      *
      * @param[in]  req   Service request.
      * @param[out] rep   Service response.
      *
      */
-    bool applyWrenchCog(skye_ros::ApplyWrenchCogNed::Request   &req,
-                        skye_ros::ApplyWrenchCogNed::Response  &rep);
+    bool applyWrenchCog(skye_ros::ApplyWrenchCogBf::Request   &req,
+                        skye_ros::ApplyWrenchCogBf::Response  &rep);
 
     /**
      * @brief      Callback function when get_link_state_ned service is called.
@@ -74,15 +76,17 @@ public:
 
 private:
     ros::NodeHandle     nh_;                          /**< Main access point to communicate with ROS. */
-    ros::Subscriber     imu_enu_subscriber_;          /**< Sub. to ENU Imu data. */
-    ros::Publisher      imu_ned_publisher_;           /**< Pub. of NED Imu data. */
-    Eigen::Quaterniond  q_ned_enu_;                   /**< Quaternion from ENU to NED frame. */
-    Eigen::Quaterniond  q_enu_ned_;                   /**< Quaternion from NED to ENU frame. */
+    ros::Subscriber     imu_gz_bf_subscriber_;        /**< Sub. to gazebo body frame Imu data. */
+    ros::Publisher      imu_bf_publisher_;            /**< Pub. of body frame Imu data. */
     ros::ServiceClient  client_gz_apply_body_wrench_; /**< Client to apply a body wrench in Gazebo. */
     ros::ServiceClient  client_gz_get_link_state_;    /**< Client to get link state in Gazebo. */
     ros::ServiceServer  server_apply_wrench_cog_;     /**< Server to apply wrench in cog of Skye. */
+    Eigen::Quaterniond  q_gzbf_bf_;                   /**< Quaternion from Skye's body frame to Gazebo body frame. */
+    Eigen::Quaterniond  q_ned_enu_;                   /**< Quaternion from ENU world frame to NED world frame. */
+    Eigen::Quaterniond  q_bf_gzbf_;                   /**< Quaternion from Gazebo body frame to Skye's body frame. */
+    Eigen::Quaterniond  q_enu_ned_;                   /**< Quaternion from NED world frame to ENU world frame. */
 
-    /* Removed server_get_link_state_ned_; gazebo provides wrong twist.angular data! 
+    /* Removed server_get_link_state_ned_; 
      * Do not allow an external user to use this service.
      * The function getLinkStateNed can be still used internally to retrieve 
      * information about the pose of one link.
@@ -111,15 +115,6 @@ private:
      */
     void quaternionMsgToEigen(const geometry_msgs::Quaternion   &quat_in,
                               Eigen::Quaterniond                &quat_out);
-
-    /**
-     * @brief      Convert IMU data from a local ENU frame attached to the IMU to local NED frame.
-     *
-     * @param[in]  imu_enu_p  imu data in local ENU frame attached to the IMU
-     * @param[out] imu_ned    imu data in local NED frame attached to the IMU
-     */
-    void imuEnuToNed(const sensor_msgs::ImuConstPtr     &imu_enu_p,
-                     sensor_msgs::Imu                   &imu_ned);
 };
 
 } // namespace skye_ros
