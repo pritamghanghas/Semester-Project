3713237223b7c6e578bfb5e290d92157f3621adf
Marco Zorzi <marcozo91@gmail.com>
2016-05-19 10:29:36 +0200

    worked on t&R, starting to develop teach mode

--- skye_teach_and_repeat/include/skye_teach_and_repeat/skye_teach_and_repeat_node.h
+++ skye_teach_and_repeat/include/skye_teach_and_repeat/skye_teach_and_repeat_node.h
@@ -2,16 +2,99 @@
 #define SKYE_TEACH_AND_REPEAT_NODE_H
 
 #include <ros/ros.h>
+#include <Eigen/Eigen>
+#include <vector>
 #include <geometry_msgs/Wrench.h>
 #include <sensor_msgs/Imu.h>
 #include <gazebo_msgs/LinkState.h>
 #include <skye_ros/ApplyWrenchCogBf.h>
+#include <std_msgs/Int16.h>
 
+struct SkyeWaypoint{
+
+  Eigen::Vector3d waypoint_position_if;
+  Eigen::Vector3d waypoint_velocity_if;
+  Eigen::Vector3d waypoint_angular_velocity_bf;
+  Eigen::Quaterniond waypoint_orientation_if;
+};
+
+struct SkyeAction{
+  int action_id;
+  std::vector<SkyeWaypoint> action_trajectory;
+};
 
 class SkyeTeachAndRepeatNode
 {
 public:
-  SkyeTeachAndRepeatNode();
+  SkyeTeachAndRepeatNode(ros::NodeHandle nh);
+  ~SkyeTeachAndRepeatNode();
+  void TeachPhase(const Eigen::Vector3d& position_if,
+                  const Eigen::Vector3d& velocity_if,
+                  const Eigen::Vector3d& angular_velocity_bf,
+                  const Eigen::Quaterniond& orientation_if);
+  void RepeatPhase();
+  void AngularVelocityCallback(const sensor_msgs::Imu::ConstPtr& msg);
+  void StateCallback(const gazebo_msgs::LinkState::ConstPtr& msg);
+  void ModeSelectionCallback(const std_msgs::Int16::ConstPtr& msg);
+  void ExecuteActionCallback(const std_msgs::Int16::ConstPtr& msg);
+  bool CallService();
+  int node_mode();
+  bool teaching_done();
+private:
+  int node_mode_;
+  int teaching_mode_; //1 for space, 2 for time
+  bool teaching_done_;
+
+  std::vector<SkyeAction> saved_data_;
+
+  //Eigen variables
+  /**
+   * @brief position_if_ : position vector expresed in the inertial frame
+   */
+  Eigen::Vector3d position_if_;
+  /**
+   * @brief velocity_if_ : linear velocity vector expressed in the inertial frame
+   */
+  Eigen::Vector3d velocity_if_;
+  /**
+   * @brief angular_velocity_bf_ :  angular velocity vector expressed in the body frame
+   */
+  Eigen::Vector3d angular_velocity_bf_;
+  /**
+   * @brief orientation_if_ : skye's orientation expressed as a quaternion
+   */
+  Eigen::Quaterniond orientation_if_;
+  /**
+   * @brief control_force_bf_ : calculated control force expressed in the body frame
+   */
+  Eigen::Vector3d control_force_bf_;
+  /**
+   * @brief control_acceleration_bf_ : calculated control acceleration expressed in the vody frame
+   */
+  Eigen::Vector3d control_acceleration_bf_;
+  /**
+   * @brief control_momentum_bf_ : calculated control momentum expressed in the body fixed frame
+   */
+  Eigen::Vector3d control_momentum_bf_;
+
+  /**
+   * @brief wrench_service_name_ : name of the service to apply a wrench to the COG of skye in body frame
+   */
+  std::string wrench_service_name_;
+
+  //ros stuff
+  /**
+   * @brief control_wrench_ : wrench created containing control force and momentum that are to be applied to Skye's cog
+   */
+  geometry_msgs::Wrench control_wrench_;
+  /**
+   * @brief wrench_service_ : the actual service object that performs the motion
+   */
+  ros::ServiceClient wrench_service_;
+  /**
+   * @brief srv_ : skye_ros service file to apply the wrench
+   */
+  skye_ros::ApplyWrenchCogBf srv_;
 };
 
 #endif // SKYE_TEACH_AND_REPEAT_NODE_H
--- skye_teach_and_repeat/src/skye_teach_and_repeat_node.cpp
+++ skye_teach_and_repeat/src/skye_teach_and_repeat_node.cpp
@@ -1,7 +1,123 @@
 #include <skye_teach_and_repeat/skye_teach_and_repeat_node.h>
 
-SkyeTeachAndRepeatNode::SkyeTeachAndRepeatNode()
-{
+SkyeTeachAndRepeatNode::SkyeTeachAndRepeatNode(ros::NodeHandle nh){
+    node_mode_ = 0;
+    teaching_done_ = false;
+    //Get the parameters
+    bool read_all_parameters = nh.getParam("wrench_service_name", wrench_service_name_) &&
+                               nh.getParam("teaching_mode", teaching_mode_);
+    // Check if Skye's parameters where imported
+    if (! read_all_parameters){
+        ROS_ERROR("Geometric Parameters not imported");
+    }
+}
+
+SkyeTeachAndRepeatNode::~SkyeTeachAndRepeatNode(){
+}
+
+void SkyeTeachAndRepeatNode::TeachPhase(const Eigen::Vector3d& position_if,
+                                        const Eigen::Vector3d& velocity_if,
+                                        const Eigen::Vector3d& angular_velocity_bf,
+                                        const Eigen::Quaterniond& orientation_if){
+    if (teaching_mode_ == 1) { //space teaching mode
+        //if just started save the first waypoint
+        if (saved_data_.size() == 0) {
+            SkyeWaypoint a_waypoint;
+            a_waypoint.waypoint_position_if = position_if;
+            a_waypoint.waypoint_velocity_if = velocity_if;
+            a_waypoint.waypoint_angular_velocity_bf = angular_velocity_bf;
+            a_waypoint.waypoint_orientation_if = orientation_if;
+
+        }
+
+
+        // check norm distance from last SkyeWaypoint
+        // Save new waypoint into SkyeAction vector
+
+
+    } else if (teaching_mode_ == 2) { //time teaching mode
+        //if just started save the first waypoint
+        // check time passed from last SkyeWaypoint
+        // Save new waypoint into SkyeAction vector
+    } else {
+        ROS_ERROR("[Skye Teach And Repeat] Teaching mode is not set correctly. Please set it in the yaml parameter file");
+    }
+
+}
+
+void SkyeTeachAndRepeatNode::RepeatPhase(){
+
+}
+
+void SkyeTeachAndRepeatNode::AngularVelocityCallback(const sensor_msgs::Imu::ConstPtr& msg){
+    // Save angular velocity from IMU topic
+    // this is in the imu frame but since it is a rigid body they are the same in terms of angular velocity
+    angular_velocity_bf_ << msg->angular_velocity.x,
+            msg->angular_velocity.y,
+            msg->angular_velocity.z;
+}
+
+void SkyeTeachAndRepeatNode::StateCallback(const gazebo_msgs::LinkState::ConstPtr& msg){
+    // pack last known position in inertial frame
+    position_if_ << msg->pose.position.x,
+            msg->pose.position.y,
+            msg->pose.position.z;
+
+    // pack last known velocity in the inertial frame
+    velocity_if_ << msg->twist.linear.x,
+            msg->twist.linear.y,
+            msg->twist.linear.z;
+
+    // pack last known quaternion in the inertial frame
+    orientation_if_.x() = msg->pose.orientation.x;
+    orientation_if_.y() = msg->pose.orientation.y;
+    orientation_if_.z() = msg->pose.orientation.z;
+    orientation_if_.w() = msg->pose.orientation.w;
+
+    if (node_mode_ == 1) {
+        // teach phase
+        this->TeachPhase(position_if_, velocity_if_, angular_velocity_bf_, orientation_if_);
+        if (!teaching_done_) teaching_done_ = true;
+    }
+    else if (node_mode_ == 2) {
+        //repeat phase
+        this->RepeatPhase();
+    }
+}
+
+void SkyeTeachAndRepeatNode::ExecuteActionCallback(const std_msgs::Int16::ConstPtr& msg){
+
+}
+
+void SkyeTeachAndRepeatNode::ModeSelectionCallback(const std_msgs::Int16::ConstPtr& msg){
+    node_mode_ = msg->data;
+}
+
+bool SkyeTeachAndRepeatNode::CallService(){
+    srv_.request.start_time.nsec = 0;
+    srv_.request.duration.sec =  1;
+
+    control_wrench_.force.x =  control_force_bf_(0);// + (*x_dist_)(generator_);
+    control_wrench_.force.y = control_force_bf_(1);// + (*y_dist_)(generator_);
+    control_wrench_.force.z = control_force_bf_(2);// + (*z_dist_)(generator_);
+    control_wrench_.torque.x = control_momentum_bf_(0);
+    control_wrench_.torque.y = control_momentum_bf_(1);
+    control_wrench_.torque.z = control_momentum_bf_(2);
+
+    srv_.request.wrench = control_wrench_;
+    if (!wrench_service_.call(srv_)){
+        ROS_ERROR("Failed to contact service. Could not pass wrench");
+        return false;
+    }
+    return true;
+}
+
+int SkyeTeachAndRepeatNode::node_mode(){
+    return node_mode_;
+}
+
+bool SkyeTeachAndRepeatNode::teaching_done(){
+    return teaching_done_;
 }
 
 int main(int argc, char **argv){
@@ -10,21 +126,36 @@ int main(int argc, char **argv){
     ros::NodeHandle nh;
 
     // Import ros parameter for service and topic names
-    std::string imu_topic, ground_truth_topic;
+    std::string mode_topic, choice_topic, imu_topic, ground_truth_topic;
     bool read_all_parameters = nh.getParam("ground_truth_topic", ground_truth_topic) &&
-            nh.getParam("imu_topic", imu_topic);
+                               nh.getParam("imu_topic", imu_topic) &&
+                               nh.getParam("choice_topic", choice_topic) &&
+                               nh.getParam("mode_topic", mode_topic);
     // Check if names have correctly been imported
     if (! read_all_parameters) ROS_ERROR("Parameters not imported");
 
-//    //Initialize node and subscribe to topics
-//    PoseControllerNode node(nh);
-//    ros::Subscriber pos_sub_ground_truth = nh.subscribe (ground_truth_topic, 1, &PoseControllerNode::PositionCallback, &node);
-//    ros::Subscriber pos_sub_imu = nh.subscribe (imu_topic, 1, &PoseControllerNode::AngularVelocityCallback, &node);
+    //Advertise topics that need the callback afterwards
+    std_msgs::Int16 zero; //(new std_msgs::Int16);
+    zero.data = 0;
+    ros::Publisher mode_sel_pub = nh.advertise<std_msgs::Int16>(mode_topic, 1);
+    ros::Publisher repeat_choice_pub = nh.advertise<std_msgs::Int16>(choice_topic, 1);
+    mode_sel_pub.publish(zero);
+    repeat_choice_pub.publish(zero);
+
+    //Initialize node and subscribe to topics
+    SkyeTeachAndRepeatNode node(nh);
+    ros::Subscriber pos_sub_ground_truth = nh.subscribe (ground_truth_topic, 1, &SkyeTeachAndRepeatNode::StateCallback, &node);
+    ros::Subscriber pos_sub_imu = nh.subscribe (imu_topic, 1, &SkyeTeachAndRepeatNode::AngularVelocityCallback, &node);
+    ros::Subscriber mode_sel_sub = nh.subscribe (mode_topic, 1, &SkyeTeachAndRepeatNode::ModeSelectionCallback, &node);
+    ros::Subscriber choice_sel_sub = nh.subscribe (choice_topic, 1, &SkyeTeachAndRepeatNode::ExecuteActionCallback, &node);
 
     ros::Rate r(50); // 50 hz
     while (nh.ok())
     {
-//        if (!node.CallService()) return -1;
+        if (node.node_mode() == 2) {
+            if (!node.CallService()) return -1;
+        }
+
         ros::spinOnce();
         r.sleep();
     }
